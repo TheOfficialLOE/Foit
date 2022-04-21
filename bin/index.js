f6f1430a9418a99ad570481f803b0af624b07606
MaxShady <max.shady1611@gmail.com>
2022-04-21 18:38:09 +0430

    Update to Foit

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 import { Command, Option, Argument } from "commander";
 const program = new Command();
 
-program.name("foid");
+program.name("foit");
 
 program
     .command("shadow")
