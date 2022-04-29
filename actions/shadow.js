757e431ef573d885251bec0f2bab68f15838e52a
MaxShady <max.shady1611@gmail.com>
2022-04-29 13:59:55 +0430

    Add stringifier

--- actions/shadow.js
+++ actions/shadow.js
@@ -1,44 +1,52 @@
 import * as fs from "fs";
 import parseCss from "../lib/parse-css.js";
+import stringify from "../lib/stringify.js";
 import isElement from "../util/is-element.js";
 import colorPref from "../util/color-settings.js";
 
 export default async (path, element, color, options) => {
+
+    if (!path.endsWith(".css"))
+        path += ".css";
 
     if (!element.startsWith(".") && !isElement(element))
         element = "." + element;
 
     color = colorPref(color, options.size);
 
-    const fileStream = fs.createReadStream(`./${path}.css`);
+    const fileStream = fs.createReadStream(`./${path}`);
 
     fileStream.on("data", chunk => {
 
         const tree = parseCss(chunk.toString());
-        let updated = "";
+        let nonFound = true;
 
         tree.forEach(selection => {
 
-            let rules = "";
-
             if (selection.selector === element) {
-                selection.rules.push({
-                   directive: "box-shadow",
-                   value:  `0 1px 8px ${color}`
+
+                selection.rules = selection.rules.filter(rule => {
+                    return rule.directive !== "box-shadow";
                 });
+
+                selection.rules.push({
+                    directive: "box-shadow",
+                    value: `0 1px 8px ${color}`
+                });
+                nonFound = false;
             }
-
-            selection.rules.forEach(rule => {
-                rules += "\t" + rule.directive + ": " + rule.value + ";\n"
-            });
-
-            updated += selection.selector + " {\n" + rules + "}\n\n"
-
 
         });
 
-        const writeStream = fs.createWriteStream(`./${path}.css`);
+        let updated = stringify(tree);
+
+        if (nonFound)
+            updated += `${element} ` + "{\n" + `\tbox-shadow: 0 1px 8px ${color}\n}\n`;
+
+        const writeStream = fs.createWriteStream(`./${path}`);
         writeStream.write(updated);
+
+        console.log("Added shadow to " + path);
 
     });
 
