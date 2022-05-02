4c9e636df4c4df125cdfee9f804c95c3a39c9e94
MaxShady <max.shady1611@gmail.com>
2022-05-03 01:23:01 +0430

    Temp migration to Mensch

--- actions/shadow.js
+++ actions/shadow.js
@@ -3,6 +3,7 @@ import isElement from "../util/is-element.js";
 import colorPref from "../util/color-settings.js";
 import shadowAction from "../util/css-action.js";
 
+import mensch from "mensch";
 
 // todo: search by element
 /* bugs:
@@ -21,16 +22,60 @@ export default async (path, element, color, options) => {
 
     const fileStream = fs.createReadStream(`./${path}`);
 
-    shadowAction(fileStream, {
-        path,
-        color,
-        element,
-        rule: {
-            directive: "box-shadow",
-            value: `0 1px 8px ${color}`
-        }
+    fileStream.on("data", chunk => {
+
+        const ast = mensch.parse(chunk.toString(), {
+            comments: true
+        });
+
+        // console.log(ast.stylesheet.rules);
+
+        ast.stylesheet.rules.some(rule => {
+            if (rule.type !== "comment") {
+                rule.selectors.forEach(selector => {
+                    if (selector === element) {
+
+                        const existing = rule.declarations.filter(
+                            declaration => declaration.type !== "comment" && declaration.name === "box-shadow"
+                        );
+
+                        if (existing && existing.length) {
+                            existing[0].value = color
+                        }
+                        else {
+                            rule.declarations.push({
+                                type: "property",
+                                name: "box-shadow",
+                                value: "red"
+                            })
+                        }
+                    }
+                })
+            }
+        });
+
+        const css = mensch.stringify(ast, {
+            comments: true,
+            indentation: "\t"
+        });
+
+        const writeStream = fs.createWriteStream(`./${path}`);
+        writeStream.write(css);
+        writeStream.close();
+        fileStream.close();
+
     });
 
-    console.log("Added shadow to " + path);
+    // shadowAction(fileStream, {
+    //     path,
+    //     color,
+    //     element,
+    //     rule: {
+    //         directive: "box-shadow",
+    //         value: `0 1px 8px ${color}`
+    //     }
+    // });
+    //
+    // console.log("Added shadow to " + path);
 
 };
