4429815cb34154f64a19f151124c85a5e16b23aa
MaxShady <max.shady1611@gmail.com>
2022-05-04 02:03:03 +0430

    Add actionPerformer

--- actions/shadow.js
+++ actions/shadow.js
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import isElement from "../util/is-element.js";
 import colorPref from "../util/color-settings.js";
-import mensch from "mensch";
+import actionPerformer from "../util/action-performer.js";
 
 export default async (path, element, color, options) => {
 
@@ -15,59 +15,13 @@ export default async (path, element, color, options) => {
 
     const fileStream = fs.createReadStream(`./${path}`);
 
-    fileStream.on("data", chunk => {
-
-        const ast = mensch.parse(chunk.toString(), {
-            comments: true
-        });
-
-        let foundSelector = false;
-
-        ast.stylesheet.rules.some(rule => {
-            if (rule.type === "rule") {
-                if (rule.selectors.includes(element)) {
-                    const existingRule = rule.declarations.filter(
-                        declaration => declaration.type !== "comment" && declaration.name === "box-shadow"
-                    );
-                    if (existingRule && existingRule.length) {
-                        existingRule[0].value = color;
-                    } else {
-                        rule.declarations.push({
-                            type: 'property',
-                            name: 'box-shadow',
-                            value: color
-                        });
-                    }
-                    foundSelector = true;
-                    return true
-                }
-            }
-        });
-
-        if (!foundSelector) {
-            ast.stylesheet.rules.push({
-                type: 'rule',
-                selectors: [element],
-                declarations: [{
-                    type: 'property',
-                    name: 'box-shadow',
-                    value: color
-                }]
-            });
-        }
-
-        const css = mensch.stringify(ast, {
-            comments: true,
-            indentation: "\t"
-        });
-
-        const writeStream = fs.createWriteStream(`./${path}`);
-        writeStream.write(css);
-        writeStream.close();
-        fileStream.close();
-
-        console.log("Added shadow to " + element);
-
+    actionPerformer(fileStream, {
+        action: "box-shadow",
+        element,
+        value: color,
+        path
     });
+
+    console.log("Added shadow to " + element);
 
 };
---
+++ util/action-performer.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+import mensch from "mensch";
+
+const actionPerformer = (fileStream, data) => {
+
+    fileStream.on("data", chunk => {
+
+        const ast = mensch.parse(chunk.toString(), {
+            comments: true
+        });
+
+        let foundSelector = false;
+
+        ast.stylesheet.rules.some(rule => {
+            if (rule.type === "rule") {
+                if (rule.selectors.includes(data.element)) {
+                    const existingRule = rule.declarations.filter(
+                        declaration => declaration.type !== "comment" && declaration.name === data.action
+                    );
+                    if (existingRule && existingRule.length) {
+                        existingRule[0].value = data.value;
+                    } else {
+                        rule.declarations.push({
+                            type: 'property',
+                            name: data.action,
+                            value: data.value
+                        });
+                    }
+                    foundSelector = true;
+                    return true
+                }
+            }
+        });
+
+        if (!foundSelector) {
+            ast.stylesheet.rules.push({
+                type: 'rule',
+                selectors: [data.element],
+                declarations: [{
+                    type: 'property',
+                    name: data.action,
+                    value: data.value
+                }]
+            });
+        }
+
+        const css = mensch.stringify(ast, {
+            comments: true,
+            indentation: "\t"
+        });
+
+        const writeStream = fs.createWriteStream(`./${data.path}`);
+        writeStream.write(css);
+        writeStream.close();
+        fileStream.close();
+
+    });
+
+
+};
+
+export default actionPerformer;
